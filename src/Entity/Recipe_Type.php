7d79e2e1ecd57c2ac48569aea38c8ec66a209f81
Jordan <suarez.jordan@orange.fr>
2020-04-10 18:25:46 +0200

    feat: create Recipe_Type model

---
+++ src/Entity/Recipe_Type.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\Entity;
+
+use Ramsey\Uuid\Uuid;
+
+class Recipe_Type
+{
+    /**
+     * @var string|null
+     */
+    private ?string $id;
+    /**
+     * @var string
+     */
+    private string $name;
+
+    /**
+     * Recipe_Type constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+}
